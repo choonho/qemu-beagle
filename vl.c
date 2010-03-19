bac69cd753e25e5e0e016fc1882b288b6190a88d
Riku Voipio <riku.voipio@nokia.com>
2010-03-19 11:19:58 +0200

    add multi-sd support
    N900 and other devices can have more than one sd/mmc card, and thus
    the -sd command line option needs to be read multiple times.

--- vl.c
+++ vl.c
@@ -2059,6 +2059,8 @@ int main(int argc, char **argv, char **envp)
     int show_vnc_port = 0;
 #endif
     int defconfig = 1;
+    int sd_device_index = 0;
+
     const char *trace_file = NULL;
 
     atexit(qemu_run_exit_notifiers);
@@ -2210,7 +2212,7 @@ int main(int argc, char **argv, char **envp)
                 drive_add(IF_MTD, -1, optarg, MTD_OPTS);
                 break;
             case QEMU_OPTION_sd:
-                drive_add(IF_SD, 0, optarg, SD_OPTS);
+                drive_add(IF_SD, sd_device_index++, optarg, SD_OPTS);
                 break;
             case QEMU_OPTION_pflash:
                 drive_add(IF_PFLASH, -1, optarg, PFLASH_OPTS);
