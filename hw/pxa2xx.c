72f9824c53be2f17a8413542d1260559887e5819
Juha Riihimäki <juha.riihimaki@nokia.com>
2010-10-26 09:23:11 +0300

    target-arm: improve board specific coprocessor support
    - pass return address to generated code to board specific
      coprocessor handler so that it is possible to generate
      exceptions from the handler function
    - make it possible to use coprocessors 0 and 1 on
      non-wmmx/xscale based boards
    
    Signed-off-by: Juha Riihimäki <juha.riihimaki@nokia.com>

--- hw/pxa2xx.c
+++ hw/pxa2xx.c
@@ -409,7 +409,8 @@ static void pxa2xx_perf_write(void *opaque, int op2, int reg, int crm,
     }
 }
 
-static uint32_t pxa2xx_cp14_read(void *opaque, int op2, int reg, int crm)
+static uint32_t pxa2xx_cp14_read(void *opaque, int op2, int reg, int crm,
+                                 void *retaddr)
 {
     switch (crm) {
     case 0:
@@ -433,7 +434,7 @@ static uint32_t pxa2xx_cp14_read(void *opaque, int op2, int reg, int crm)
 }
 
 static void pxa2xx_cp14_write(void *opaque, int op2, int reg, int crm,
-                uint32_t value)
+                uint32_t value, void *retaddr)
 {
     switch (crm) {
     case 0:
