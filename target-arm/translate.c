433dba41114d6ea531c395de8847b7c21a23c74a
Peter Maydell <peter.maydell@linaro.org>
2011-01-18 15:51:19 +0000

    target-arm: Pull pairwise = 0 out of switch
    Pull the setting of pairwise = 0 out of the switch so we don't need
    to repeat it in all the cases in this switch. This simplifies the
    following patches which add extra cases.

--- target-arm/translate.c
+++ target-arm/translate.c
@@ -4301,6 +4301,7 @@ static int disas_neon_data_insn(CPUState * env, DisasContext *s, uint32_t insn)
                 return 1;
             }
         }
+        pairwise = 0;
         switch (op) {
         case 8: /* VSHL */
         case 9: /* VQSHL */
@@ -4312,7 +4313,6 @@ static int disas_neon_data_insn(CPUState * env, DisasContext *s, uint32_t insn)
                 rtmp = rn;
                 rn = rm;
                 rm = rtmp;
-                pairwise = 0;
             }
             break;
         case 20: /* VPMAX */
@@ -4327,7 +4327,6 @@ static int disas_neon_data_insn(CPUState * env, DisasContext *s, uint32_t insn)
             pairwise = u;
             break;
         default:
-            pairwise = 0;
             break;
         }
 
