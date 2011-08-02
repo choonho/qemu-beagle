390360f7f7c27b04e0279784c56400a7f396a9f5
Peter Maydell <peter.maydell@linaro.org>
2011-08-02 13:00:52 +0100

    hw/omap_gpmc.c: Use OMAP_RO_REGV/OMAP_BAD_REGV
    Might choose to drop this later (it's overspill from
    gpmc cleanup)

--- hw/omap_gpmc.c
+++ hw/omap_gpmc.c
@@ -705,7 +705,7 @@ static void omap_gpmc_write32(void *opaque, target_phys_addr_t addr,
     case 0x200 ... 0x220:	/* GPMC_ECC_RESULT */
     case 0x234:	/* GPMC_PSA_LSB */
     case 0x238:	/* GPMC_PSA_MSB */
-        OMAP_RO_REG(addr);
+        OMAP_RO_REGV(addr, value);
         break;
 
     case 0x010:	/* GPMC_SYSCONFIG */
@@ -889,7 +889,7 @@ static void omap_gpmc_write32(void *opaque, target_phys_addr_t addr,
 
     default:
     bad_reg:
-        OMAP_BAD_REG(addr);
+        OMAP_BAD_REGV(addr, value);
         return;
     }
 }
@@ -944,7 +944,7 @@ static void omap_gpmc_write8(void *opaque, target_phys_addr_t addr,
         break;
     default:
     bad_reg:
-        OMAP_BAD_REG(addr);
+        OMAP_BAD_REGV(addr, value);
         return;
     }
 }
@@ -1008,7 +1008,7 @@ static void omap_gpmc_write16(void *opaque, target_phys_addr_t addr,
         break;
     default:
     bad_reg:
-        OMAP_BAD_REG(addr);
+        OMAP_BAD_REGV(addr, value);
         return;
     }
 }
